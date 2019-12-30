fe3a08e2fd7875dd9e6d66643d00ab9a405d51f5
gaojianhui <958226322@qq.com>
2019-12-30 11:13:54 +0800

    问题

--- src/main/java/com/gaojianhui/framework/GaojianhuiServerApplication.java
+++ src/main/java/com/gaojianhui/framework/GaojianhuiServerApplication.java
@@ -1,18 +1,25 @@
 package com.gaojianhui.framework;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.SpringApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
-@MapperScan("com.gaojianhui.framework.dtomapper")
 @SpringBootApplication
+@MapperScan(basePackages= {"com.gaojianhui.framework.mapper"})
+@EntityScan(basePackages={"com.gaojianhui.framework.model"})
+@EnableJpaRepositories(basePackages = {"com.gaojianhui.framework.dao"})
+//@EnableCaching
+@ServletComponentScan
 public class GaojianhuiServerApplication {
 	@Bean
 	RestTemplate restTemplate() {
 		RestTemplate rest = new RestTemplate();
-		rest.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
+//		rest.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
 		return rest;
 	}
 	public static void main(String[] args) {
